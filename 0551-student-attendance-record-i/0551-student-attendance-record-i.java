d9fa1dd19a295d7b34f93ecf8acc4dcf81bbf215
YashGupta068 <yashgupta6806@gmail.com>
2026-08-26 20:58:20 +0530

    Time: 0 ms (100%), Space: 43 MB (42.55%) - LeetHub

--- 0551-student-attendance-record-i/0551-student-attendance-record-i.java
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.java
@@ -1,24 +1,19 @@
 class Solution {
     public boolean checkRecord(String s) {
-
-        int countA = 0;
-
-        for (int i = 0; i < s.length(); i++) {
-
-            if (s.charAt(i) == 'A') {
-                countA++;
-
-                if (countA >= 2) {
-                    return false;
-                }
+         int c = 0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='A'){
+                c++;
             }
 
-            if (i >= 2 &&
-                s.charAt(i - 2) == 'L' &&
-                s.charAt(i - 1) == 'L' &&
-                s.charAt(i) == 'L') {
+            if(c>=2){
                 return false;
             }
+
+            if(i>=2 && s.charAt(i-2) == 'L' && s.charAt(i-1) =='L' && s.charAt(i)=='L'){
+                return false;
+            }
+
         }
 
         return true;
