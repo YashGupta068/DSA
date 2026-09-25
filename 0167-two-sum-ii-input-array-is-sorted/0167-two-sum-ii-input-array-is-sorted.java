b42eb8d31ca56f60be2a4ce75274ba25da161479
YashGupta068 <yashgupta6806@gmail.com>
2026-09-25 09:30:40 +0530

    Time: 3 ms (18.65%), Space: 48.3 MB (79.54%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -2,15 +2,30 @@ class Solution {
     public int[] twoSum(int[] arr, int target) {
         int n = arr.length;
 
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(arr[i]+arr[j] == target){
-                    return new int[]{i + 1, j + 1};
-                }
+        int res[] = new int[2];
+
+        int i = 0;
+        int j = n - 1;
+
+        while(i<j){
+            if(arr[i] + arr[j] > target){
+                j--;
+            }
+            if(arr[i] + arr[j] < target){
+                i++;
+            }
+            if(arr[i] + arr[j] == target){
+                res[0] = ++i;
+                res[1] = ++j;
+                break;
             }
         }
         
-        return new int[]{};
+        return res;
 
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
