1151dac0dae2310feb6a22c22b715d01aa8a0e8a
YashGupta068 <yashgupta6806@gmail.com>
2026-09-25 09:39:35 +0530

    Time: 11 ms (32.25%), Space: 46.5 MB (19.44%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,19 +1,22 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-
-        int i =0;
+        s = s.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
+        int i = 0;
         int j = s.length()-1;
 
         while(i<j){
-            char c = s.charAt(i);
-            char h = s.charAt(j);
-            if(c!=h){
+            if(s.charAt(i) != s.charAt(j)){
                 return false;
             }
             i++;
             j--;
         }
+
         return true;
+
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
